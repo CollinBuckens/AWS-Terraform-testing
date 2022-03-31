f094a9e820119570af343c85fd9fbd8d383cfe18
collinbuckens <collin.buckens@student.vives.be>
2022-03-31 16:01:54 +0200

    added code for the first terraform init

---
+++ private-bastion-with-nat-gateway/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-west-3" # paris
+  default_tags { # Tags that are applied by default on every module.
+    tags = {
+      Terraform = true
+      Environment = "${module.shared_vars.env_suffix}"
+    }
+  }
+}
+
+module "shared_vars" {
+  source = "./modules/shared_vars_module"
+}

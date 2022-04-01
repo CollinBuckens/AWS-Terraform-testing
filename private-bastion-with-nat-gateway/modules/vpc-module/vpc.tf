44d67ea9d80afd525e2d408675f5e7c00ca0da40
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 16:16:14 +0200

    fixed subnets in vpc

--- private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
+++ private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
@@ -1,12 +1,12 @@
 module "aws_vpc" {
   source                                         = "terraform-aws-modules/vpc/aws"
   create_vpc                                     = true
-  cidr                                           = "10.0.0.0/16"
+  cidr                                           = "10.1.0.0/16"
   azs                                            = ["eu-west-3a", "eu-west-3b"]
-  public_subnets                                 = ["10.0.1.0/24"]
-  private_subnets                                = ["10.1.0.0/24"]
-  enable_ipv6                                    = true
-  assign_ipv6_address_on_creation                = true
+  public_subnets                                 = ["10.1.0.0/24","10.1.1.0/24"]
+  private_subnets                                = ["10.1.2.0/24","10.1.3.0/24"]
+  enable_ipv6                                    = false
+  assign_ipv6_address_on_creation                = false
   private_subnet_assign_ipv6_address_on_creation = false
 
   tags = {

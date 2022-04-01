60b3caf44166d69b840b980895848928d37209ce
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 15:56:22 +0200

    added name tag

--- private-bastion-with-nat-gateway/modules/nat-gateway-module/nat-gateway.tf
+++ private-bastion-with-nat-gateway/modules/nat-gateway-module/nat-gateway.tf
@@ -1,4 +1,8 @@
 resource "aws_nat_gateway" "private_nat_gateway" {
   connectivity_type = "private"
   subnet_id = var.subnet_id
+
+  tags = {
+    "name" = "private_nat_gateway_${var.env_suffix}"
+  }             
 }

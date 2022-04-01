81656cde326556f062b2470687b5a1f5e8d2a6ca
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:33:19 +0200

    fixed typo in vpc.tf

--- private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
+++ private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
@@ -2,8 +2,8 @@ module "aws_vpc" {
   source                                         = "terraform-aws-modules/vpc/aws"
   create_vpc                                     = true
   cidr                                           = "10.0.0.0/16"
-  public_subnets                                 = "10.0.1.0/24"
-  private_subnets                                = "10.1.0.0/24"
+  public_subnets                                 = ["10.0.1.0/24"]
+  private_subnets                                = ["10.1.0.0/24"]
   enable_ipv6                                    = true
   assign_ipv6_address_on_creation                = true
   private_subnet_assign_ipv6_address_on_creation = false

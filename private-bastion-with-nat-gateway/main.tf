57abae69375371e03beba185e147977b4d1d8c69
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:16:04 +0200

    added bastion and vpc module to main

--- private-bastion-with-nat-gateway/main.tf
+++ private-bastion-with-nat-gateway/main.tf
@@ -11,3 +11,25 @@ provider "aws" {
 module "shared_vars" {
   source = "./modules/shared-vars"
 }
+
+module "VPC_module" {
+  source     = "./modules/VPC_module"
+  env_suffix = module.shared_vars.env_suffix
+  depends_on = [
+    module.shared_vars,
+  ]
+}
+
+module "bastion_module" {
+  source                    = "./modules/bastion-module"
+  env_suffix                = module.shared_vars.env_suffix
+  subnet_id                 = module.VPC_module.aws_vpc_public_subnet[0]
+  VPC_security_group_ids    = module.security_groups_module.security_group_bastion_id
+  iam_instance_profile_name = module.iam_module.profile_name
+  depends_on = [
+    module.security_groups_module,
+    module.VPC_module,
+    module.shared_vars,
+    module.iam_module
+  ]
+}

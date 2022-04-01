2a155375f26a7faf2a83944aa1b92555a2b232a4
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:58:38 +0200

    fixed dependencies in bastion.tf

--- private-bastion-with-nat-gateway/modules/bastion-module/bastion.tf
+++ private-bastion-with-nat-gateway/modules/bastion-module/bastion.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.vpc_security_group_ids]
-  iam_instance_profile   = var.iam_instance_profile
+  iam_instance_profile   = var.iam_instance_profile_name
 
   tags = {
     "Name" = "Bastion_Host-${var.env_suffix}"

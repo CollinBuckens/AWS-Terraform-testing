b762fee0d6f2c20bd53bd13acdc813aee7ae28d9
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:23:58 +0200

    terraform fmt

--- private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
+++ private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
@@ -1,14 +1,14 @@
 module "aws_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  create_vpc = true
-  cidr = "10.0.0.0/16"
-  public_subnets = "10.0.1.0/24"
-  private_subnets = "10.1.0.0/24"
-  enable_ipv6 = true
-  assign_ipv6_address_on_creation = true
+  source                                         = "terraform-aws-modules/vpc/aws"
+  create_vpc                                     = true
+  cidr                                           = "10.0.0.0/16"
+  public_subnets                                 = "10.0.1.0/24"
+  private_subnets                                = "10.1.0.0/24"
+  enable_ipv6                                    = true
+  assign_ipv6_address_on_creation                = true
   private_subnet_assign_ipv6_address_on_creation = false
 
   tags = {
-      name = "vpc_${var.env_suffix}"
+    name = "vpc_${var.env_suffix}"
   }
 }

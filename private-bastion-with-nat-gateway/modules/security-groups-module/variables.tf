d35036c3c37f0b412736ab3f1a5a0a138986ebeb
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 15:03:05 +0200

    added variables to security groups

---
+++ private-bastion-with-nat-gateway/modules/security-groups-module/variables.tf
@@ -0,0 +1,12 @@
+variable "env_suffix" {
+  description = "The environment suffix"
+}
+variable "aws_vpc_id" {
+  description = "the id of the VPC"
+}
+variable "aws_vpc_cidr" {
+  description = "the cidr of the VPC for ipv4"
+}
+variable "aws_vpc_cidr_ipv6" {
+  description = "the cidr of the VPC for ipv6"
+}

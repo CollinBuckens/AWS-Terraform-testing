76e3ddf3f42b3b36df5a5871ae60e502ab3ac45c
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:56:59 +0200

    added more modules and fixed some typos in main.tf

--- private-bastion-with-nat-gateway/main.tf
+++ private-bastion-with-nat-gateway/main.tf
@@ -24,12 +24,32 @@ module "bastion_module" {
   source                    = "./modules/bastion-module"
   env_suffix                = module.shared_vars.env_suffix
   subnet_id                 = module.VPC_module.aws_vpc_public_subnet[0]
-  VPC_security_group_ids    = module.security_groups_module.security_group_bastion_id
+  vpc_security_group_ids    = module.security_groups_module.security_group_bastion_id
   iam_instance_profile_name = module.iam_module.profile_name
   depends_on = [
     module.security_groups_module,
     module.VPC_module,
     module.shared_vars,
     module.iam_module
+  ]
+}
+
+module "security_groups_module" {
+  source            = "./modules/security-groups-module"
+  env_suffix        = module.shared_vars.env_suffix
+  aws_vpc_id        = module.VPC_module.aws_vpc_id
+  aws_vpc_cidr      = module.VPC_module.aws_vpc_cidr
+  aws_vpc_cidr_ipv6 = module.VPC_module.aws_vpc_cidr_ipv6
+  depends_on = [
+    module.VPC_module,
+    module.shared_vars
+  ]
+}
+
+module "iam_module" {
+  source     = "./modules/iam-module"
+  env_suffix = module.shared_vars.env_suffix
+  depends_on = [
+    module.shared_vars
   ]
 }

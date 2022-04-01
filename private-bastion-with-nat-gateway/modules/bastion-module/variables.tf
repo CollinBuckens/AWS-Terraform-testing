cd66d0f2da3c0742cf07b8cacd20ce78f4f582d9
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:17:38 +0200

    added variables for the bastion

---
+++ private-bastion-with-nat-gateway/modules/bastion-module/variables.tf
@@ -0,0 +1,12 @@
+variable "env_suffix" {
+  description = "The environment suffix"
+}
+variable "subnet_id" {
+  description = "The id of the subnet"
+}
+variable "VPC_security_group_ids" {
+  description = "The id of the security group of the VPC"
+}
+variable "iam_instance_profile_name" {
+  description = "The name of the iam instance profile"
+}

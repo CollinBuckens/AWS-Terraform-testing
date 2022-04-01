20f25d6eeb590e998964b1c3cbbe340ff5f1bd22
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:16:46 +0200

    configured the bastion

---
+++ private-bastion-with-nat-gateway/modules/bastion-module/bastion.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "bastion" {
+  ami = "ami-0960de83329d12f2f"
+  instance_type = "t2.micro"
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [var.vpc_security_group_ids]
+  iam_instance_profile = var.iam_instance_profile
+
+  tags = {
+    "Name" = "Bastion_Host-${var.env_suffix}"
+  }
+}

a7db070c35c65d21d067a361e7784a7a88a1f95e
CollinBuckens <collin.buckens@pegus.digital>
2022-04-01 14:32:54 +0200

    fixed typo in outputs.tf in vpc-module

--- private-bastion-with-nat-gateway/modules/vpc-module/outputs.tf
+++ private-bastion-with-nat-gateway/modules/vpc-module/outputs.tf
@@ -1,25 +1,25 @@
 # enter the outputs here
 output "aws_vpc_id" {
   description = "The id of the gateway VPC"
-  value       = module.aws_aws_vpc.vpc_id
+  value       = module.aws_vpc.vpc_id
 }
 
 output "aws_vpc_cidr" {
   description = "The cidr block of the gateway VPC"
-  value       = module.aws_aws_vpc.vpc_cidr_block
+  value       = module.aws_vpc.vpc_cidr_block
 }
 
 output "aws_vpc_ipv6" {
   description = "The ipv6 cidr block of the gateway VPC"
-  value       = module.aws_aws_vpc.vpc_ipv6_cidr_block
+  value       = module.aws_vpc.vpc_ipv6_cidr_block
 }
 
 output "aws_vpc_public_subnet" {
   description = "The public subnet of the VPC"
-  value       = module.aws_aws_vpc.public_subnets
+  value       = module.aws_vpc.public_subnets
 }
 
 output "aws_vpc_private_subnet" {
   description = "The private subnet of the VPC"
-  value       = module.aws_aws_vpc.private_subnets
+  value       = module.aws_vpc.private_subnets
 }

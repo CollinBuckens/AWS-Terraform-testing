13c16d1eb99a590b61a632e6972eb003c2fb3595
CollinBuckens <collin.buckens@pegus.digital>
2022-04-04 10:07:51 +0200

    made changes in the vpc module

--- private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
+++ private-bastion-with-nat-gateway/modules/vpc-module/vpc.tf
@@ -3,8 +3,8 @@ module "aws_vpc" {
   create_vpc                                     = true
   cidr                                           = "10.1.0.0/16"
   azs                                            = ["eu-west-3a", "eu-west-3b"]
-  public_subnets                                 = ["10.1.0.0/24","10.1.1.0/24"]
-  private_subnets                                = ["10.1.2.0/24","10.1.3.0/24"]
+  public_subnets                                 = ["10.1.0.0/24", "10.1.1.0/24"]
+  private_subnets                                = ["10.1.2.0/24", "10.1.3.0/24"]
   enable_ipv6                                    = false
   assign_ipv6_address_on_creation                = false
   private_subnet_assign_ipv6_address_on_creation = false
@@ -13,3 +13,4 @@ module "aws_vpc" {
     name = "vpc_${var.env_suffix}"
   }
 }
+

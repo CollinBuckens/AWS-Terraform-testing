7345e34ca55ec6926b492b01d0f9744e5a470169
CollinBuckens <collin.buckens@pegus.digital>
2022-04-05 17:11:57 +0200

    linked correct instance profile

--- EC2-instance-jenkins-host/modules/ec2-module/ec2.tf
+++ EC2-instance-jenkins-host/modules/ec2-module/ec2.tf
@@ -9,8 +9,9 @@ module "ec2_instance" {
   monitoring             = true
   vpc_security_group_ids = [var.vpc_security_group_ids]
   subnet_id              = var.subnet_id
+  iam_instance_profile   = var.iam_instance_profile_name
 
   tags = {
-      name = "Jenkins-host-${var.env_suffix}"
+    name = "Jenkins-host-${var.env_suffix}"
   }
 }

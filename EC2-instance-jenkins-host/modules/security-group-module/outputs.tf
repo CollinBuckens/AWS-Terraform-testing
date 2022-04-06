c9d79e18fb480b57fdd19f045336961fe7f865d2
CollinBuckens <collin.buckens@pegus.digital>
2022-04-06 16:10:47 +0200

    added comment to test pipeline

--- EC2-instance-jenkins-host/modules/security-group-module/outputs.tf
+++ EC2-instance-jenkins-host/modules/security-group-module/outputs.tf
@@ -1,3 +1,4 @@
+# the outputs of the security groups.
 output "security_group_jenkins_id" {
   description = "The id of the security group for the bastion"
   value       = try(aws_security_group.Security_Group_jenkins.id, "")

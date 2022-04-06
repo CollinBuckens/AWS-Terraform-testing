a1b20d23c699d29ab0b7a5d84b5499451699d3f7
CollinBuckens <collin.buckens@pegus.digital>
2022-04-06 16:32:26 +0200

    added comment to test pipeline

--- EC2-instance-jenkins-host/modules/iam-module/profiles.tf
+++ EC2-instance-jenkins-host/modules/iam-module/profiles.tf
@@ -1,3 +1,4 @@
+# creation of the jenkins host profile
 resource "aws_iam_instance_profile" "jenkins_host_profile" {
   name = "jenkins_host_profile"
   role = resource.aws_iam_role.jenkins_host_role.name
